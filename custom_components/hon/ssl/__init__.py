0503780f1b2200bc619bb87d642b3428b74180a4
Jan Kozak <galvani78@gmail.com>
2025-03-07 15:08:59 +0100

    attempt release fixes to ssl #7

--- custom_components/hon/ssl/__init__.py
+++ custom_components/hon/ssl/__init__.py
@@ -11,7 +11,7 @@ try:
     from homeassistant.core import HomeAssistant
 except ImportError:
     print("Home Assistant not found. Running standalone.")
-    HomeAssistant = None
+    HomeAssistantType = None  # Use a different variable name to avoid confusion
 
 _LOGGER = logging.getLogger(__name__)
 
